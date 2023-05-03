4a9aa2c74df13fe06d38daea67933d67fbd5d006
derwind <uncertainty_principle_ss@yahoo.co.jp>
2023-05-04 00:50:52 +0900

    enable --detect-missing-symbols

--- auto_typehints.sh
+++ auto_typehints.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python -B auto_typehints.py --qiskit-root qiskit --inplace quantum_info
+python -B auto_typehints.py --qiskit-root qiskit --inplace quantum_info --detect-missing-symbols
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --detect-missing-symbols
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --only statevector.py one_qubit_decompose.py operator.py --verbose
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --only operator.py --verbose
