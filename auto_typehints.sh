f5b2653e8909a3e8657289226514988ce0a906ab
derwind <uncertainty_principle_ss@yahoo.co.jp>
2023-05-03 15:15:06 +0900

    change default command

--- auto_typehints.sh
+++ auto_typehints.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 python -B auto_typehints.py --qiskit-root qiskit --inplace quantum_info
-#python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted
+#python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --verbose --detect-missing-symbols
+#python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --only statevector.py --verbose
