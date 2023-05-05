7b5a7ee52d3bd60b999ac65e9ef35db8bb6fde41
derwind <uncertainty_principle_ss@yahoo.co.jp>
2023-05-05 19:21:10 +0900

    update default command

--- auto_typehints.sh
+++ auto_typehints.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+python -B auto_typehints.py --qiskit-root qiskit --inplace quantum_info --detect-missing-symbols --enhance-missing-symbols-treatment
 #python -B auto_typehints.py --qiskit-root qiskit --inplace quantum_info --detect-missing-symbols
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --detect-missing-symbols
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --only statevector.py one_qubit_decompose.py operator.py --verbose
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --only operator.py --verbose
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --only one_qubit_decompose.py --verbose --detect-missing-symbols
 #python -B auto_typehints.py --qiskit-root qiskit quantum_info --suffix .hinted --only z2_symmetries.py --verbose --detect-missing-symbols
-python -B auto_typehints.py --qiskit-root qiskit --inplace quantum_info --detect-missing-symbols --only-dir states
+#python -B auto_typehints.py --qiskit-root qiskit --inplace quantum_info --detect-missing-symbols --only-dir states
